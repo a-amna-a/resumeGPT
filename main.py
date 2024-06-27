df82c05bf385548339547276793de672ad2cffb5
Anthony Rojas <anthony.rojas@utexas.edu>
2024-06-27 14:46:43 -0500

    Added Database Functionality (user can store previous resumes)

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import os
 import openai
+import pandas as pd
 from openai import OpenAI
 from pypdf import PdfReader
+from sqlalchemy import create_engine
 
 
 # Ask user for a resume file; read the text from the resume file
@@ -26,18 +28,68 @@ def read_job_desc(file_name):
         return job_desc
 
 
+def prompt_resume():
+    user_answer = input("Do you want to use a previous resume (p) or upload a \
+                         new resume (n)? ")
+    if user_answer == "n":
+        resume_content = read_resume(input("Enter the file path to your \
+                                            resume (pdf): "))
+        # If the file path is bogus, just exit
+        if resume_content is None:
+            print("Invalid File. Please Try again.")
+            exit()
+        save = input("Do you want to save this resume (y/n)? ")
+        if save == "y":
+            name = input("What do you want to name this resume? ")
+            # Add ['name', 'content'] to resumes table
+            engine = create_engine('sqlite:///resume_database.db')
+            new_info = pd.DataFrame([[name, resume_content]],
+                                    columns=['name', 'content'])
+            new_info.to_sql('resumes', con=engine, if_exists='append',
+                            index=False)
+
+        elif save != "n":
+            print("Invalid response. Please Try again.")
+            exit()
+    elif user_answer == "p":
+        # Display the resume ids and names to the console
+        engine = create_engine('sqlite:///resume_database.db')
+        sql = "SELECT * FROM resumes"
+        df = pd.read_sql(sql, con=engine)
+
+        print()
+        print(df["name"].to_string())
+        print()
+
+        id_string = input("What resume id would you like to use? ")
+        if not id_string.isnumeric():
+            print("Invalid response. Please Try again.")
+            exit()
+        resume_id = int(id_string)
+        if resume_id < 0 or resume_id >= df.shape[0]:
+            print("Invalid response. Please Try again.")
+            exit()
+
+        # Grab the resume content from the table using the id provided
+        resume_content = df['content'].iloc[resume_id]
+
+    else:
+        print("Invalid response. Please Try again.")
+        exit()
+
+    return resume_content
+
+
 if __name__ == "__main__":
-    resume_file_name = input("Enter the file path to your resume (pdf): ")
+    resume = prompt_resume()
+
     job_file_name = input("Enter the file path to your job description: ")
-
-    resume = read_resume(resume_file_name)
     job = read_job_desc(job_file_name)
-
-    if resume is None or job is None:
+    if job is None:
         print("Invalid file(s), try again.")
         exit()
 
-    # We need to set up the connection to ChatGPT
+    # set up the connection to ChatGPT
     my_api_key = os.getenv('OPENAI_KEY')
     openai.api_key = my_api_key
 
